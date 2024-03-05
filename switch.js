ff4b621ecfa9346edc20e7a64e286fe6d8963fd5
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-05 14:48:21 -0300

    testes aula switch

--- switch.js
+++ switch.js
@@ -28,3 +28,52 @@ imprimirResultado(0)
 imprimirResultado(-1)
 imprimirResultado(7)
 imprimirResultado('erro')
+
+// Testes ↓↓↓↓
+
+const mesesAno = function (mes) {
+    switch ((mes)) {
+        case 1:
+            console.log('Janeiro')
+            break
+        case 2:
+            console.log('Fevereiro')
+            break
+        case 3:
+            console.log('Março')
+            break
+        case 4:
+            console.log('Abril')
+            break
+        case 5:
+            console.log('Maio')
+            break
+        case 6:
+            console.log('Junho')
+            break
+        case 7:
+            console.log('Julho')
+            break
+        case 8:
+            console.log('Agosto')
+            break
+        case 9:
+            console.log('Setembro')
+            break
+        case 10:
+            console.log('Outubro')
+            break
+        case 11:
+            console.log('Novembro')
+            break
+        case 12:
+            console.log('Dezembro')
+            break
+        default:
+            console.log('Insira um mês valido.')
+    }
+}
+
+mesesAno(6)
+mesesAno(9)
+mesesAno(2)

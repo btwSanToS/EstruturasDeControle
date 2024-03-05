fcfc7a6dda908c034c67424e6afea866d567fe96
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-04 21:39:16 -0300

    aprendizado curso

---
+++ ifElseIf.js
@@ -0,0 +1,33 @@
+Number.prototype.entre = function (inicio, fim) {
+    return this >= inicio && this <= fim
+}
+
+const imprimirResultado = function (nota) {
+    if (nota.entre(9, 10)) {
+        console.log('Quadro de honra!')
+    }
+    else if (nota.entre(7, 8.99)) {
+        console.log('Aprovado!')
+    }
+    else if (nota.entre(4, 6.99)) {
+        console.log('Recuperação.')
+    }
+    else if (nota.entre(0, 3.99)) {
+        console.log('Reprovado.')
+    }
+    else {
+        console.log('Nota Inválida!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(5)
+imprimirResultado(7)
+imprimirResultado(4.5)
+imprimirResultado(3.2)
+imprimirResultado(-1)
+
+/* 
+A utilização de if -- else if será realizada para cada bloco, caso o último não seja TRUE
+Nesses casos, caso a sentença atual retorne FALSE, ela irá para a sentença abaixo.
+*/

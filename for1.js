81a383be8d1e56c9655cb5a42c587f0f3673f55c
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-11 13:25:30 -0300

    aprendizado curso

---
+++ for1.js
@@ -0,0 +1,27 @@
+let contador = 1
+while (contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador++
+}
+
+for (i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [7.9, 5.7, 9.4, 6.2]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`notas = ${notas[i]}`)
+}
+
+// Testes ↓↓↓↓
+
+const pessoas = ['André', 'Rodrigo', 'Enzo', 'Erick', 'Clóvis']
+for (let p = 0; p < pessoas.length; p++) {
+    console.log(`As pessoas inseridas são: ${pessoas[p]}`)
+}
+
+
+const frutas = ['abacaxi', 'manga', 'kiwi', 'pêssego'];
+for (let f = 0; f < frutas.length; f++) {
+    console.log(`Fruta = ${frutas[f]}`);
+}

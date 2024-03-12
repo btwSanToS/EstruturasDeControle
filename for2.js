7a3e4073b20a84abda40510600e74c0fc62bd009
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-11 21:22:02 -0300

    melhoria de vida

--- for2.js
+++ for2.js
@@ -1,7 +1,6 @@
 //For In ↓↓↓↓
 
 const notas = [7.8, 5.3, 4.1, 9.8, 6.3]
-
 for (let n in notas) {
     console.log(n, notas[n]) // Chamando o indíce e o valor
 }
@@ -16,13 +15,11 @@ Com a vírgula, pode-se inserir o valor, chamando o mesmo.
 
 
 const notas1 = [7.8, 5.3, 4.1, 9.8, 6.3]
-
 for (let n1 in notas) {
     console.log(notas[n1]) // Chamando apenas os valores.
 }
 
 const notas2 = [7.8, 5.3, 4.1, 9.8, 6.3]
-
 for (let n2 in notas) {
     console.log(n2) // Chamando apenas os indíces
 }
@@ -34,7 +31,6 @@ const pessoa = {
     idade: '23',
     peso: '86',
 }
-
 for (let atributos in pessoa) {
     console.log(`${atributos} = ${pessoa[atributos]}`)
 }               //   Indíce        // Valor Indíce
@@ -45,7 +41,18 @@ const pessoa2 = {
     Idade: '23',
     Peso: '60'
 }
-
 for (let atributos2 in pessoa2) {
     console.log(`${atributos2} = ${pessoa2[atributos2]}`)
+}
+
+//////////////////////////////////////////////////////////////////
+
+const empresas = {
+    FabricanteCarros: 'Audi',
+    EmpresaTecnologia: 'Google',
+    HiperMercado: 'Carrefour',
+    Multinacional: 'Burguer King'
+}
+for (let nomes in empresas) {
+    console.log(`${nomes} = ${empresas[nomes]}`)
 }
